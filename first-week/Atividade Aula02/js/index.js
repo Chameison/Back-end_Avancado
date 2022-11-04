173bb05008d6b9314a95f7c0505dd1e5ae0e7284
Chameison <chameison.acre@gmail.com>
2022-11-03 20:15:59 -0500

    Aula 02

---
+++ first-week/Atividade Aula02/js/index.js
@@ -0,0 +1,24 @@
+
+let num1 = document.querySelector('#num1');
+let num2 = document.querySelector('#num2');
+let opera = document.querySelector('#opera');
+let form = document.querySelector("#form");
+console.log(num1)
+console.log(num2)
+console.log(opera)
+let num3 = 10
+console.log(num3)
+
+function resultado() {
+    if(opera == '+') {
+        console.log(num1+num2);
+    }
+    if(opera == '-'){
+    
+    }
+}
+document.getElementById.onclick = function(e) {
+    alert(resultado())
+}
+
+https://pt.stackoverflow.com/questions/469931/n%C3%A3o-consigo-pegar-valor-do-input-javascript
--- first-week/js/index.js
+++ first-week/js/index.js
@@ -1,3 +1,97 @@
+
+//Aula 01
 console.log('texto')
 console.error('Essa é uma mensagem de erro')
 console.warn('Isso é um alerta!')
+
+//Aula 02 
+//variaveis
+var primeiro
+let segundo
+const terceiro = 1
+
+let x = 10
+if(true) {
+    let x = 20
+}
+
+console.log(x);
+
+//string
+const name = 'Chameison'
+console.log(name)
+console.log(typeof name)
+//number
+const num = 10;
+console.log(num)
+console.log(typeof num)
+
+//bool 
+const isOpen = true //geralmente quando começa com is é bool
+
+console.log(isOpen)
+console.log(typeof isOpen)
+
+obj = {
+    nome : 'chameison',
+    idade : '19',
+    altura : 1.69
+};
+//object
+console.log(obj)
+console.log(typeof obj)
+console.log(obj.idade)
+
+//null
+const a = null;
+console.log(a)
+console.log(typeof a)
+
+//undefined
+
+let nada
+console.log(nada)
+console.log(typeof nada)
+
+//array 
+
+const linguagem = ['C++', 'Java', 'Python', 'JS']
+console.log(typeof linguagem)
+console.log(linguagem)
+console.log(linguagem[3])
+console.log(typeof linguagem[1])
+
+// object literals
+const user = {
+    username: 'Chameison',
+    password: '*****',
+    age: 20,
+    name: 'Chameison'
+}
+
+console.log(user)
+
+
+//metodos de string
+const word = 'qualquer um'
+console.log(word.length)
+
+//quebrar texto em um conjunto de caracteres
+const textToArray = word.split('quer')
+console.log(textToArray[1])
+console.log(word.toLocaleUpperCase)
+
+//procurar algo dentro de uma string
+console.log(word.indexOf('abc'))
+console.log(word.charAt(0))
+console.log(word.slice(1,4))
+
+//mais sobre arrays
+
+const chars = ['AAA', 'B', 'C', 'D']
+console.log(chars.length)
+console.log(chars[chars.length-1])
+chars[5] = 'F'
+console.log(chars)
+chars.pop() //sempre remove no final
+chars.unshift('NEW')
