9acb3bf722b34d5bda14a744212ece1454da5e1a
Chameison <chameison.acre@gmail.com>
2022-11-10 19:08:38 -0500

    enviando alguns arquivos

--- aula06/node-day-one/scripts.js
+++ aula06/node-day-one/scripts.js
@@ -39,11 +39,12 @@
 // console.log('Segundo comando')
 // console.log('Terceiro comando')
 
-setTimeout(myFunction, 3000)
 
-function myFunction() {
-    console.log('testando a função...')
-}
+// setTimeout(myFunction, 3000)
+
+// function myFunction() {
+//     console.log('testando a função...')
+// }
 
 // //forma resumida
 // //funao anonima
@@ -53,26 +54,97 @@ function myFunction() {
 // console.log('comando apos o set')
 
 //ATIVIDADE 05 
-function pegarData (){
-    const date = new Date(Date.now())
-    const day = date.getDay();
-    const month = date.getMonth()
-    const year = date.getFullYear()
-    const hora = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    console.log(`O dia é ${day} do mes ${month} no ano de ${year}`)
-    console.log(`a data atual é : ${date}`)
-    console.log(`a hora atual é: ${hora}`)
-    console.log(`os minutos atuais são: ${minutes}`)
-    console.log(`Os segundos atuais sao: ${seconds}`)
-}
-setTimeout(pegarData, 5000)
+// function pegarData (){
+//     const date = new Date(Date.now())
+//     const day = date.getDay();
+//     const month = date.getMonth()
+//     const year = date.getFullYear()
+//     const hora = date.getHours();
+//     const minutes = date.getMinutes();
+//     const seconds = date.getSeconds();
+//     console.log(`O dia é ${day} do mes ${month} no ano de ${year}`)
+//     console.log(`a data atual é : ${date}`)
+//     console.log(`a hora atual é: ${hora}`)
+//     console.log(`os minutos atuais são: ${minutes}`)
+//     console.log(`Os segundos atuais sao: ${seconds}`)
+// }
+// setTimeout(pegarData, 5000)
 
-setTimeout(pegarData, 2000)
-setTimeout(pegarData, 5000)
+// setTimeout(pegarData, 2000)
+// setTimeout(pegarData, 5000)
 
 // const data = new Date('November 09, 2022 20:12:30');
 // const hours = data.getHours();
 // const minutes = data.getMinutes();
 // const seconds = data.getSeconds();
+
+//AULA 07 - 10/11/2022
+//Utilizando programação assincrona
+
+function sum(){
+    
+    const result = 1 + 1
+    if(result === 2){
+        sucess()
+    }else{
+        erro()
+    }
+}
+
+function sucess() {
+    console.log(`O resultado da soma é 2`)
+}
+function erro(){
+    console.log(`A soma não é o resultado esperado`)
+}
+sum()
+//o fato por exemplo de que o consol
+
+//O CODIGO ACIMA NÃO TEM PROGRAMAÇÃO ASSINCRONA
+
+//vamos transforma para callback
+
+function sum2(callback, callbackErro, vetor) {
+    const result2 = 1 + 1
+    if(result2 === 2) {
+        const vetor = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+            1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+            1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+            1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+            1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+            1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+            1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+            1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+            1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+            1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+            1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+            1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+            1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+            1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+        callback()
+    }else{
+        callbackErro()
+    }
+
+}
+
+sum2(
+    () => (console.log('Sucesso, é 2') ),
+    () => (console.log('Erro!')),
+    () => (console.log(vetor))
+)
+console.log('Testando o tempo')
+
+
+const promisse = new Promise((resolve, reject) => {
+    const result3 = 1 + 1
+    if(result3 === 2) {
+        resolve()
+    }else{
+        reject()
+    }
+
+})
+
+promisse.then((resultado)=>(console.log(resultado)))
+promisse.catch((resultado)=>(console.log(resultado)))
